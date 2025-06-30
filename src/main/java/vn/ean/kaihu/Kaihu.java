e3e709b4c6762b701969aebe21c30bf4de989e4d
ean <52894214+ean161@users.noreply.github.com>
2025-06-30 21:52:38 +0700

    add broadcast response and run command as console

--- src/main/java/vn/ean/kaihu/Kaihu.java
+++ src/main/java/vn/ean/kaihu/Kaihu.java
@@ -1,11 +1,13 @@
 package vn.ean.kaihu;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 
 import okhttp3.*;
@@ -13,6 +15,7 @@ import okhttp3.*;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import net.md_5.bungee.api.ChatColor;
 
 public class Kaihu implements Listener {
     private static final OkHttpClient client = new OkHttpClient();
@@ -20,12 +23,16 @@ public class Kaihu implements Listener {
     @EventHandler
     public void onChat(AsyncChatEvent event) {
         String message = PlainTextComponentSerializer.plainText().serialize(event.message());
+
+        if (!message.startsWith("@kaihu") && !message.contains("kaihu"))
+            return;
+
         Player player = event.getPlayer();
 
         try {
             String encodedPlayer = URLEncoder.encode(player.getName(), StandardCharsets.UTF_8.name());
             String encodedIsOp = URLEncoder.encode(String.valueOf(player.isOp()), StandardCharsets.UTF_8.name());
-            String encodedMsg = URLEncoder.encode(message, StandardCharsets.UTF_8.name());
+            String encodedMsg = URLEncoder.encode(message.replace("@kaihu", ""), StandardCharsets.UTF_8.name());
 
             String query = String.format("player=%s&isOp=%s&msg=%s", encodedPlayer, encodedIsOp, encodedMsg);
 
@@ -41,14 +48,34 @@ public class Kaihu implements Listener {
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
                     String body = response.body().string();
+                    final String[] answer = {""};
+                    final String[] command = {""};
+
+                    if (body.contains("[split_param]")) {
+                        String[] parts = body.split("\\[split_param\\]", 2);
+                        answer[0] = parts[0].trim();
+                        command[0] = parts[1].trim();
+                    } else {
+                        answer[0] = body.trim();
+                    }
 
                     Bukkit.getScheduler().runTask(App.getInstance(), () -> {
-                        player.sendMessage(body);
+                        if (!answer[0].isEmpty()) {
+                            Bukkit.broadcast(Component.text(ChatColor.translateAlternateColorCodes('&', String.format(
+                                "%s %s",
+                                "&7[&b&lKaihu&r&7]&f",
+                                answer[0]
+                            ))));
+                            // Bukkit.broadcast(Component.text(answer[0]));
+                        }
+
+                        if (!command[0].isEmpty()) {
+                            ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+                            Bukkit.dispatchCommand(console, command[0].startsWith("/") ? command[0].substring(1) : command[0]);
+                        }
                     });
                 }
             });
-        } catch (Exception e) {
-            player.sendMessage("Đã xảy ra lỗi khi gửi tin nhắn.");
-        }
+        } catch (Exception e) { }
     }
 }
