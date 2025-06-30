8230efc721e46452e0aaed7fc25a16e0e73242d2
ean <52894214+ean161@users.noreply.github.com>
2025-06-30 18:22:04 +0700

    add ask request

--- src/main/java/vn/ean/kaihu/App.java
+++ src/main/java/vn/ean/kaihu/App.java
@@ -2,13 +2,16 @@ package vn.ean.kaihu;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class App extends JavaPlugin implements Listener {
     Kaihu kaihu = new Kaihu();
+    private static App instance;
 
     @Override
     public void onEnable() {
+        instance = this;
         Bukkit.getPluginManager().registerEvents(kaihu, this);
         getLogger().info("Kaihu enabled, developed by ean (nghoaian161@gmail.com)");
     }
@@ -16,5 +19,9 @@ public class App extends JavaPlugin implements Listener {
     @Override
     public void onDisable() {
         getLogger().info("Kaihu disabled");
+    }
+
+    public static Plugin getInstance() {
+        return instance;
     }
 }
--- src/main/java/vn/ean/kaihu/Kaihu.java
+++ src/main/java/vn/ean/kaihu/Kaihu.java
@@ -1,5 +1,6 @@
 package vn.ean.kaihu;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,11 +8,42 @@ import org.bukkit.event.Listener;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 
+import okhttp3.*;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 public class Kaihu implements Listener {
+    private static final OkHttpClient client = new OkHttpClient();
+
     @EventHandler
     public void onChat(AsyncChatEvent event) {
         String message = PlainTextComponentSerializer.plainText().serialize(event.message());
         Player player = event.getPlayer();
         
+        String encodedMsg = String.format("player=%s&isOp=%s&msg=%s", player.getName(), player.isOp() ? "true" : "false", message);
+        try {
+            encodedMsg = URLEncoder.encode(message, StandardCharsets.UTF_8.name());
+        } catch (Exception ex) { }
+
+        Request request = new Request.Builder()
+            .url("https://ean.vn/models/project/kaihu/ask.php?" + encodedMsg)
+            .get()
+            .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) { }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String body = response.body().string();
+
+                Bukkit.getScheduler().runTask(App.getInstance(), () -> {
+                    player.sendMessage(body.length() > 200 ? body.substring(0, 200) + "..." : body);
+                });
+            }
+        });
     }
 }
